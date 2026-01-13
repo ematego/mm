dce38ff13cc66706ad318e4fd9ef7ac136893d2a
ematego <emat1900aura@gmail.com>
2026-01-13 13:58:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.style.opacity = '0';
+    loader.style.pointerEvents = 'none';
+    setTimeout(() => loader.remove(), 1000);
+  });
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
